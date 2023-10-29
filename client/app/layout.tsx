dd3dbbf6583db942376c903c05634f974a693fed
ljq0226 <1035821043@qq.com>
2023-10-29 14:51:16 +0800

    feat:add lint

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,8 @@
-
 export default function Home() {
   return (
-    <div className='bg-black w-20 h-20 underline'>123 
-    <div className="dark w-10 h-10 bg-primary"></div>
-    
-    
+    <div className="bg-black w-20 h-20 underline">
+      123
+      <div className="dark w-10 h-10 bg-primary"></div>
 
     </div>
   )
