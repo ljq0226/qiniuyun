c67b1cd9c6c3c2ea77b8df7527a1a706cc76d1fc
ljq0226 <1035821043@qq.com>
2023-11-05 13:46:47 +0800

    config: log & cors &pipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+import { logger } from './common/middleware/logger.middleware'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true })
+  app.enableCors()
+  app.use(logger)
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(8000)
 }
-bootstrap();
+bootstrap()
